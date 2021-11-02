c09ea4e1553bd8ce1ecb4c10a1e9c8baa172b31d
antoneta.safta <safta.antoneta@gmail.com>
2021-11-02 12:01:31 +0200

    ss
    ss

--- _site/assets/js/toggle.js
+++
@@ -1,7 +0,0 @@
-function toggleNavbar(collapseID) {
-    document.getElementById(collapseID).classList.toggle("hidden");
-    document.getElementById(collapseID).classList.toggle("bg-white");
-    document.getElementById(collapseID).classList.toggle("m-2");
-    document.getElementById(collapseID).classList.toggle("py-3");
-    document.getElementById(collapseID).classList.toggle("px-6");
-  }
